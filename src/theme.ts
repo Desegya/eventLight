b760ea70ce0c4197bf42c947f533f2186cc08154
Desmond Egya <egyadesmond@gmail.com>
2024-12-26 03:45:38 +0100

    Change the initial color mode to system

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { extendTheme, ThemeConfig } from "@chakra-ui/react";
 
 const config: ThemeConfig = {
-  initialColorMode: "dark",
+  initialColorMode: "system",
 };
 
 const theme = extendTheme({
@@ -20,9 +20,6 @@ const theme = extendTheme({
       900: "#111",
     },
   },
-   breakpoints: {
-    "xxl": "96em"
-   },
 });
 
 export default theme;
